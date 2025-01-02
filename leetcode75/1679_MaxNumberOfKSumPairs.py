a3f853ab6136152e616e20fda265a7ea450f7727
Rishabh Kaushick <kaushick.r@northeastern.edu>
2025-01-01 21:11:16 -0500

    Added daily leetcode75 challenges

---
+++ leetcode75/1679_MaxNumberOfKSumPairs.py
@@ -0,0 +1,13 @@
+# 1679: Max Number of K-Sum Pairs
+
+# Difficulty: Medium
+
+# Accepted: 403.2K
+# Submissions: 725.5K
+# Acceptance Rate: 55.6%
+
+class Solution:
+    pass
+
+if __name__ == '__main__':
+    pass
